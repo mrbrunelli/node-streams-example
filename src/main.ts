0bdca3dc421548479c44f4f042fb859f2d44ffd9
mrbrunelli <matheus.brunelli@gmail.com>
2023-03-27 18:48:48 -0300

    add express example

--- src/main.ts
+++ src/main.ts
@@ -1,59 +1,75 @@
-import { time, timeEnd } from "node:console";
+import express from "express";
 import { randomUUID } from "node:crypto";
-import { createWriteStream } from "node:fs";
+import { createReadStream, createWriteStream } from "node:fs";
+import fs from "node:fs/promises";
 import { Readable, Transform } from "node:stream";
 import { pipeline } from "node:stream/promises";
 import { QuotesAPI } from "./services/QuotesAPI.js";
 
-time("Get API data");
-const quotesAPI = new QuotesAPI();
-const quotes = await quotesAPI.getQuotes();
-timeEnd("Get API data");
+const app = express();
+app.use(express.json());
 
-time("Streams");
-const loadQuotes = new Readable({
-  read() {
-    for (const quote of quotes) {
-      this.push(JSON.stringify(quote));
-    }
-    this.push(null);
-  },
+app.get("/generate", async (req, res) => {
+  const quotesAPI = new QuotesAPI();
+  const quotes = await quotesAPI.getQuotes();
+
+  const loadQuotes = new Readable({
+    read() {
+      for (const quote of quotes) {
+        this.push(JSON.stringify(quote));
+      }
+      this.push(null);
+    },
+  });
+
+  const addTimestamp = new Transform({
+    transform(chunk, encoding, callback) {
+      const dto = JSON.parse(chunk);
+      const quote = {
+        ...dto,
+        createdAt: new Date(),
+      };
+      callback(null, JSON.stringify(quote));
+    },
+  });
+
+  const addUUID = new Transform({
+    transform(chunk, encoding, callback) {
+      const dto = JSON.parse(chunk);
+      const quote = {
+        ...dto,
+        id: randomUUID(),
+      };
+      callback(null, JSON.stringify(quote));
+    },
+  });
+
+  const addLinebreak = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, `${chunk}\n`);
+    },
+  });
+
+  pipeline(
+    loadQuotes,
+    addTimestamp,
+    addUUID,
+    addLinebreak,
+    createWriteStream("output.txt")
+  );
+
+  return res.status(200).send("Streams generated successfully");
 });
 
-const addTimestamp = new Transform({
-  transform(chunk, encoding, callback) {
-    const dto = JSON.parse(chunk);
-    const quote = {
-      ...dto,
-      createdAt: new Date(),
-    };
-    callback(null, JSON.stringify(quote));
-  },
+app.get("/streams", async (req, res) => {
+  await pipeline(createReadStream("output.txt"), res);
 });
 
-const addUUID = new Transform({
-  transform(chunk, encoding, callback) {
-    const dto = JSON.parse(chunk);
-    const quote = {
-      ...dto,
-      id: randomUUID(),
-    };
-    callback(null, JSON.stringify(quote));
-  },
+app.get("/fs", async (req, res) => {
+  const txt = await fs.readFile("output.txt");
+  return res.send(txt.toString());
 });
 
-const addLinebreak = new Transform({
-  transform(chunk, encoding, callback) {
-    callback(null, `${chunk}\n`);
-  },
+app.listen(3000, () => {
+  console.log("Server is running on 3000");
 });
-
-await pipeline(
-  loadQuotes,
-  addTimestamp,
-  addUUID,
-  addLinebreak,
-  createWriteStream("output.txt")
-);
-
-timeEnd("Streams");
